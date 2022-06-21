2fe6451fd2d0e6417512ec00de0c5735a3306330
Nikolai Lvov <nikolailvov@Nikolais-MacBook-Pro.local>
2022-06-21 11:27:57 -0700

    filled server

--- api/server.js
+++ api/server.js
@@ -3,5 +3,16 @@ const express = require("express")
 const server = express()
 
 // DO YOUR MAGIC
+server.use(express.json());
+
+server.use('*', (req, res) => {
+    next({ status: 404, message: 'not found' })
+})
+
+server.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        message: err.message
+    })
+})
 
 module.exports = server
